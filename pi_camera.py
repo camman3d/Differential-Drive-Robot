439136862c72031eda8f1ca872e78a949b7d8399
Joshua Monson <you@example.com>
2014-04-07 11:46:51 +0000

    Modified pi camera command

--- pi_camera.py
+++ pi_camera.py
@@ -5,5 +5,7 @@ __author__ = 'josh'
 
 def read():
     # Perform pi camera read
-    os.system("/opt/vc/bin/raspicam -w 800 -h 600 -t 0 -o /tmp/capture.jpg")
+    #os.system("/opt/vc/bin/raspicam -w 800 -h 600 -t 0 -o /tmp/capture.jpg")
+    #os.system("/usr/bin/raspistill -w 800 -h 600 -o /tmp/capture.jpg -t 1")
+    os.system("/usr/bin/raspistill -n -w 800 -h 600 -o /tmp/capture.jpg -awb auto -t 1 -ss 200000")
     return "/tmp/capture.jpg"
