d805e20cf701907777aca8e8797e8c7d7fd31d40
Josh <camman3d@gmail.com>
2014-04-26 20:37:42 -0400

    Changed fuzzy controller to incorporate behavior states

--- fuzzy/controller.py
+++ fuzzy/controller.py
@@ -10,54 +10,87 @@ DoNothing = 0, 0, 0
 # Fuzzy rules
 
 def nothing(fuzzy_vars):
-    if fuzzy_vars[0] is None and fuzzy_vars[1] is None:
-        return TurnLeft
+    return fuzzy_vars[0] is None and fuzzy_vars[1] is None
 
 
 def dest_middle(fuzzy_vars):
-    if fuzzy_vars[1] is not None and 0.3 < fuzzy_vars[1] < 0.7 and fuzzy_vars[2] < 0.5:
-        return MoveForward
+    return fuzzy_vars[1] is not None and 0.3 < fuzzy_vars[1] < 0.7 and fuzzy_vars[2] < 0.75
 
 
 def dest_left(fuzzy_vars):
-    if fuzzy_vars[1] is not None and fuzzy_vars[1] <= 0.3 and fuzzy_vars[2] < 0.5:
-        return TurnLeft
+    return fuzzy_vars[1] is not None and fuzzy_vars[1] <= 0.3 and fuzzy_vars[2] < 0.75
 
 
 def dest_right(fuzzy_vars):
-    if fuzzy_vars[1] is not None and fuzzy_vars[1] >= 0.7 and fuzzy_vars[2] < 0.5:
-        return TurnRight
+    return fuzzy_vars[1] is not None and fuzzy_vars[1] >= 0.7 and fuzzy_vars[2] < 0.75
 
 
-def obst_midleft(fuzzy_vars):
-    if fuzzy_vars[0] is not None and 0.2 < fuzzy_vars[0] <= 0.5 < fuzzy_vars[2]:
-        return TurnRight
+# def obst_left(fuzzy_vars):
+#     return fuzzy_vars[0] is not None and fuzzy_vars[0] < 0.5 < fuzzy_vars[2]
+#
+#
+# def obst_right(fuzzy_vars):
+#     return fuzzy_vars[0] is not None and fuzzy_vars[0] > 0.5 and fuzzy_vars[2] > 0.5
+#
+#
+# def obst_midleft(fuzzy_vars):
+#     return fuzzy_vars[0] is not None and 0.2 < fuzzy_vars[0] <= 0.5 < fuzzy_vars[2]
+#
+#
+# def obst_midright(fuzzy_vars):
+#     return fuzzy_vars[0] is not None and 0.5 < fuzzy_vars[0] < 0.8 and fuzzy_vars[2] > 0.5
 
 
-def obst_midright(fuzzy_vars):
-    if fuzzy_vars[0] is not None and 0.5 < fuzzy_vars[0] < 0.8 and fuzzy_vars[2] > 0.5:
-        return TurnLeft
+def obst(fuzzy_vars):
+    return fuzzy_vars[0] is not None and fuzzy_vars[2] > 0.75
 
 
-def obst_left(fuzzy_vars):
-    if fuzzy_vars[0] is not None and fuzzy_vars[0] <= 0.2 and fuzzy_vars[2] > 0.5:
-        return MoveForward
+# def obst_left(fuzzy_vars):
+#     return fuzzy_vars[0] is not None and fuzzy_vars[0] <= 0.2 and fuzzy_vars[2] > 0.5
 
 
-def obst_right(fuzzy_vars):
-    if fuzzy_vars[0] is not None and fuzzy_vars[0] >= 0.8 and fuzzy_vars[2] > 0.5:
-        return MoveForward
+# def obst_right(fuzzy_vars):
+#     return fuzzy_vars[0] is not None and fuzzy_vars[0] >= 0.8 and fuzzy_vars[2] > 0.5
 
 
 # Rule processor
 
-all_rules = [nothing, dest_left, dest_middle, dest_right, obst_left, obst_midleft, obst_midright, obst_right]
+# all_rules = [nothing, dest_left, dest_middle, dest_right, obst_left, obst_midleft, obst_midright, obst_right]
+#
+#
+# def processor(fuzzy_vars, rules):
+#     result = 0, 0, 0
+#     for rule in rules:
+#         r = rule(fuzzy_vars)
+#         if r is None:
+#             r = DoNothing
+#         result = result[0] + r[0], result[1] + r[1], result[2] + r[2]
+#     return result
 
-def processor(fuzzy_vars, rules):
-    result = 0, 0, 0
-    for rule in rules:
-        r = rule(fuzzy_vars)
-        if r is None:
-            r = DoNothing
-        result = result[0] + r[0], result[1] + r[1], result[2] + r[2]
-    return result
+
+def get_behavior(fuzzy_vars, last_behavior):
+    if nothing(fuzzy_vars):
+        if last_behavior <= 0:
+            return last_behavior - 1
+        if 4 <= last_behavior <= 7:
+            return last_behavior + 2
+        if last_behavior >= 8:
+            return last_behavior
+
+    if dest_middle(fuzzy_vars):
+        return 1
+    if dest_left(fuzzy_vars):
+        return 2
+    if dest_right(fuzzy_vars):
+        return 3
+
+    if obst(fuzzy_vars):
+        if 4 <= last_behavior <= 5:
+            return last_behavior
+        if last_behavior == 8:
+            return 4
+        if last_behavior == 9:
+            return 5
+        return 4
+
+    return 0
--- main2.py
+++ main2.py
@@ -1,39 +1,57 @@
 from fuzzy import controller
 import image_processor
-from raspberrypi.robot_agent import RobotAgent
-#from simulation.vision_simulation_agent import VisionSimulationAgent
+# from raspberrypi.robot_agent import RobotAgent
+from simulation.vision_simulation_agent import VisionSimulationAgent
 
 __author__ = 'josh'
 
 # agent = VisionSimulationAgent([256, 256, 0], True, "env1.blend")
 #agent = VisionSimulationAgent([256, 256, 0], True, "env2.blend")
-# agent = VisionSimulationAgent([256, 256, 0], True, "env3.blend")
+agent = VisionSimulationAgent([256, 256, 0], True, "env3.blend")
 
-agent = RobotAgent()
+# agent = RobotAgent()
 
+behaviors = {
+    "-2": "Explore by moving",
+    "-1": "Explore by turning",
+    "0": "Explore by turning",
+    "1": "Go forward to goal",
+    "2": "Turn left to goal",
+    "3": "Turn right to goal",
+    "4": "Turn left to go around obj",
+    "5": "Turn right to go around obj",
+    "6": "Go forward around obj",
+    "7": "Go forward around obj",
+    "8": "Turn right back to obj",
+    "9": "Turn left back to obj"
+}
 
-turn_granularity = 0.5
+turn_granularity = 0.3
 move_distance = 5
+last_behavior = -1
+last_turn = agent.rotate_left
 
 while True:
     img = agent.get_image()
     fuzzy_vars = image_processor.process(img, agent.obst_hue, agent.dest_hue, agent.image_width)
     print("Fuzzy vars: " + str(fuzzy_vars))
-    results = controller.processor(fuzzy_vars, controller.all_rules)
-    print("Results: " + str(results))
 
-    # Execute the action
-    action = 0, -1
-    for i in range(3):
-        if results[i] > action[0]:
-            action = results[0], i
+    behavior = controller.get_behavior(fuzzy_vars, last_behavior)
+    print("Behavior: " + behaviors[str(behavior)])
+    last_behavior = behavior
 
-    if action[1] is 0:
-        print("Moving forward")
+    if behavior <= 0:
+        last_turn(turn_granularity)
+    if behavior <= -2:
+        last_behavior = 0
         agent.move_forward(move_distance)
-    elif action[1] is 1:
-        print("Turning left")
+    elif behavior == 1 or behavior == 6 or behavior == 7:
+        agent.move_forward(move_distance)
+    elif behavior == 2 or behavior == 4 or behavior == 9:
         agent.rotate_left(turn_granularity)
-    elif action[1] is 2:
-        print("Turning right")
+        last_turn = agent.rotate_left
+    elif behavior == 3 or behavior == 5 or behavior == 8:
         agent.rotate_right(turn_granularity)
+        last_turn = agent.rotate_right
+
+
